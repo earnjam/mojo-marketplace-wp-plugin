d755718b1329848d0d32775c6fc6789ec0fac747
Mike <web.mhansen@gmail.com>
2016-12-15 15:11:30 -0700

    Cleanup ajax functions.

--- inc/performance.php
+++ inc/performance.php
@@ -49,15 +49,10 @@ function mm_cache_toggle() {
 add_action( 'wp_ajax_mm_cache', 'mm_cache_toggle' );
 
 function mm_php_edge_toggle() {
-if ( isset( $_POST['type'] ) && isset( $_POST['current_status'] ) ) {
-		$defaults = array(
-			'php_edge'  => 'disabled',
-		);
-		$cache_settings = get_option( 'mm_php_edge_settings' );
-		$cache_settings = wp_parse_args( $php_edge_settings, $defaults );
-		$valid_php_edge_names = array(
-			'php_edge',
-		);
+	if ( isset( $_POST['current_status'] ) ) {
+
+		$php_edge_settings = get_option( 'mm_php_edge_settings', 'disabled' );
+
 		$valid_status = array(
 			'enabled',
 			'disabled',
@@ -69,22 +64,16 @@ if ( isset( $_POST['type'] ) && isset( $_POST['current_status'] ) ) {
 			$response = array( 'status' => 'error', 'message' => 'Invalid status.' );
 		}
 
-		if ( in_array( $_POST['type'], $valid_php_edge_names ) && ! isset( $response ) ) {
+		if ( ! isset( $response ) ) {
 			if ( 'enabled' == $new_status ) {
-				$response = mm_cache_add( $_POST['type'] );
+				$response = mm_php_edge_add();
 			} else {
-				$response = mm_cache_remove( $_POST['type'] );
+				$response = mm_php_edge_remove( );
 			}
-			if ( 'object' != $_POST['type'] && 'success' == $response['status'] ) {
-				save_mod_rewrite_rules();
-			}
-		} else {
-			$response = array( 'status' => 'error', 'message' => 'Invalid cache type.' );
 		}
 
 		if ( 'success' == $response['status'] ) {
-			$php_edge_settings[ $_POST['type'] ] = $new_status;
-			update_option( 'mm_cache_settings', $cache_settings );
+			update_option( 'mm_php_edge_settings', $new_status );
 		}
 		echo json_encode( $response );
 	}
@@ -158,44 +147,43 @@ function mm_cache_remove( $type = null ) {
 }
 
 function mm_php_edge_add( $type = null ) {
-	$php7 = array();
+	$php_edge = array();
 	if ( ! is_dir( WP_CONTENT_DIR . '/mu-plugins' ) ) {
 		mkdir( WP_CONTENT_DIR . '/mu-plugins' );
 	}
-	switch ( $type ) {
-		case 'browser':
-			$cache['code'] = 'https://raw.githubusercontent.com/bluehost/endurance-php-edge/master/endurance-php-edge.php';
-			$cache['location'] = WP_CONTENT_DIR . '/mu-plugins/endurance-php-edge.php';
-			break;
-	}
-	if ( isset( $php_edge['code'] ) && isset( $php_edge['location'] ) ) {
-		$request = wp_remote_get( $php_edge['code'] );
-		if ( ! is_wp_error( $request ) ) {
-			file_put_contents( $php_edge['location'], $request['body'] );
-			if ( file_exists( $php_edge['location'] ) ) {
-				$response = array( 'status' => 'success', 'message' => ucfirst( $type ) . ' php edge updated successfully.' );
+	$php_edge['code'] = 'https://raw.githubusercontent.com/bluehost/endurance-php-edge/master/endurance-php-edge.php';
+	$php_edge['location'] = WP_CONTENT_DIR . '/mu-plugins/endurance-php-edge.php';
+
+	$request = wp_remote_get( $php_edge['code'] );
+	if ( ! is_wp_error( $request ) ) {
+		file_put_contents( $php_edge['location'], $request['body'] );
+		save_mod_rewrite_rules();
+		if ( file_exists( $php_edge['location'] ) ) {
+			$php_check = wp_remote_get( site_url() );
+			if ( ! is_wp_error( $php_check ) && wp_remote_retrieve_response_code( $php_check ) == '200' ) {
+				$response = array( 'status' => 'success', 'message' => 'PHP Edge enabled successfully.' );
+			} else {
+				$response = array( 'status' => 'error', 'message' => 'Site is incompatible with PHP Edge. Edge removed.' );
+				mm_php_edge_remove();
+				save_mod_rewrite_rules();
 			}
 		}
-	}
-	if ( ! isset( $response ) ) {
-		$response = array( 'status' => 'error', 'message' => 'Unable to add ' . ucfirst( $type ) . ' php edge.' );
+	} else {
+		$response = array( 'status' => 'error', 'message' => 'Unable to enable PHP Edge.' );
 	}
 	return $response;
 }
 function mm_php_edge_remove( $type = null ) {
-	switch ( $type ) {
-		case 'php7':
-			$file = WP_CONTENT_DIR . '/mu-plugins/endurance-php-edge.php';
-			break;
-	}
+	$file = WP_CONTENT_DIR . '/mu-plugins/endurance-php-edge.php';
+
 	if ( file_exists( $file ) ) {
 		if ( unlink( $file ) ) {
-			$response = array( 'status' => 'success', 'message' => ucfirst( $type ) . ' php edge successfully removed.' );
+			$response = array( 'status' => 'success', 'message' => 'PHP Edge successfully disabled.' );
 		} else {
-			$response = array( 'status' => 'error', 'message' => 'Could not remove php edge.' );
+			$response = array( 'status' => 'error', 'message' => 'Unable to remove PHP Edge.' );
 		}
 	} else {
-		$response = array( 'status' => 'error', 'message' => 'php edge file does not exist.' );
+		$response = array( 'status' => 'error', 'message' => 'PHP Edge file does not exist.' );
 	}
 	return $response;
 }
